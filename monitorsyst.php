b6fdfc407b05503bbb4f2f51015d06874e3030b2
Alexey Shait <ashaitarov@hotmail.com>
2021-06-14 20:31:58 +0300

    added iframe for loganalyzer and mrtg

--- monitorsyst.php
+++ monitorsyst.php
@@ -1,9 +1,9 @@
 						<div class="vtabs">
-							  <div id="content1-1">
-								Contents 1...<br>
-								Contents 1...<br>
+							  <div id="loganalyzer_tab">
+							  	<?php $host = 'https://' . $_SERVER['SERVER_NAME'] . '/loganalyzer'; ?>
+								<iframe src="<?php echo $host; ?>" style="height: -webkit-fill-available; width: 100%;" align="left">You're browser does not support IFRAME</iframe>
 							  </div>
-							  <div id="content1-2">
+							  <div id="mrtg_tab">
 								Contents 2...<br>
 								Contents 2...<br>
 							  </div>
@@ -14,8 +14,8 @@
 								Contents 4...
 							  </div>
 							  <div class="vtabs__links">
-								<a href="#content1-1">LogAnalyzer</a>
-								<a href="#content1-2">MRTG</a>
+								<a href="#loganalyzer_tab">LogAnalyzer</a>
+								<a href="#mrtg_tab">MRTG</a>
 								<a href="#content1-3">DarkStat</a>
 								<a href="#content1-4">Modbus RTU/ASCII/TCP</a>
 							  </div>
